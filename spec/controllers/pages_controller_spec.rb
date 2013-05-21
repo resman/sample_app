55931e0d17494c6fd5a486ade9df42d652510f3b
Regan Chorlog <regan@chorlog.com>
2013-05-21 08:31:14 -0600

    Added Stylesheets and instance variable

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,11 @@ require 'spec_helper'
 describe PagesController do
   render_views
   
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+  
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -12,15 +17,16 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                      :content => "Ruby on Rails Tutorial Sample App | Home")
+                                    :content => "#{@base_title} | Home")
       end
+      
       it "should have a non-blank body" do
         get 'home'
         response.body.should_not =~ /<body>\s*<\/body>/
                     
       end
     end
-
+    
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
@@ -30,9 +36,9 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                      :content => "Ruby on Rails Tutorial Sample App | Contact")
-    end
-  end
+                                    :content => "#{@base_title} | Contact")
+      end
+  end 
   
   describe "GET 'about'" do
     it "should be successful" do
@@ -43,7 +49,7 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-                      :content => "Ruby on Rails Tutorial Sample App | About")
+                                    :content => "#{@base_title} | About")
     end
   end   
 end
