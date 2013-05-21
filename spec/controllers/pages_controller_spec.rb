79f8c8b3791466a783bbaa7a336b0b0e815212ea
Regan Chorlog <regan@chorlog.com>
2013-05-21 11:02:00 -0600

    Added pages controller spec

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -37,7 +37,7 @@ describe PagesController do
       get 'contact'
       response.should have_selector("title",
                                     :content => "#{@base_title} | Contact")
-      end
+    end
   end 
   
   describe "GET 'about'" do
@@ -50,6 +50,19 @@ describe PagesController do
       get 'about'
       response.should have_selector("title",
                                     :content => "#{@base_title} | About")
+  end
+end
+                                    
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+                                    :content => "#{@base_title} | Help")
     end
   end   
 end
