50ee8c607b623c6c469fefee3cc98d5be29abbb6
Regan Chorlog <regan@chorlog.com>
2013-05-22 17:39:43 -0600

    Finished Password Machinery

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,15 +2,22 @@ require 'spec_helper'
 
 describe UsersController do
   render_views
+
+  describe "GET 'show'" do
+    it "should be successful" do
+      get :show
+      response.show be_success
+    end
+  end
   
   describe "GET 'new'" do
     it "returns http success" do
-      get 'new'
+      get :new
       response.should be_success
     end
     
     it "should have the right title" do
-      get 'new'
+      get :new
       response.should have_selector('title', :content => "Sign up")
     end
   end
